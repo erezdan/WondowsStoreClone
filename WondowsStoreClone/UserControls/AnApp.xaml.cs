79a2cc3cee136e1cabe4267c2e486a1d9d9197be
erez dan <erezdanmail@gmail.com>
2021-08-03 12:49:32 +0300

    79

---
+++ WondowsStoreClone/UserControls/AnApp.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MiscUtil;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WondowsStoreClone.UserControls
+{
+    public partial class AnApp : UserControl
+    {
+        public string AppName;
+        public ImageSource AppImageSource;
+
+        public AnApp()
+        {
+            InitializeComponent();
+
+            List<string> filePaths = Directory.GetFiles(Environment.CurrentDirectory + @"\..\..\Images\", "*.png").ToList();
+            FileInfo randomFile = new FileInfo(filePaths[StaticRandom.Next(filePaths.Count)]);
+            ProductImage.Source = new BitmapImage(new Uri(randomFile.FullName, UriKind.RelativeOrAbsolute));
+            AppNameText.Text = (new CultureInfo("en-US", false).TextInfo).ToTitleCase(randomFile.Name.Split('-').Last().Split('.').First());
+
+            AppName = AppNameText.Text;
+            AppImageSource = ProductImage.Source;
+        }
+
+        private void ProductImage_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WondowsStoreClone/UserControls/AppsViewer.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WondowsStoreClone.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AppsViewer.xaml
+    /// </summary>
+    public partial class AppsViewer : UserControl
+    {
+        List<AnApp> PresentedApps;
+
+        public AppsViewer()
+        {
+            InitializeComponent();
+
+            PresentedApps = new List<AnApp>();
+            AppsList.ItemsSource = PresentedApps;
+            for (int ii = 0; ii < 9; ii++)
+            {
+                PresentedApps.Add(new AnApp());
+            }
+        }
+
+        private void ScrollLeft_Click(object sender, RoutedEventArgs e)
+        {
+            int widthOfOneApp = (int)(PresentedApps.First().ActualWidth + 2 * PresentedApps.First().Margin.Left);
+            AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset - 2 * widthOfOneApp);
+        }
+
+        private void ScrollRight_Click(object sender, RoutedEventArgs e)
+        {
+            int widthOfOneApp = (int)(PresentedApps.First().ActualWidth + 2 * PresentedApps.First().Margin.Left);
+            AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset + 2 * widthOfOneApp);
+        }
+    }
+}
